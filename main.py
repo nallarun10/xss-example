648371211d3ffdade2499a195b039eb2cd5377a4
Arun Nalla <nalla.arunkumar@gmail.com>
2019-05-28 20:23:44 -0700

    Class08 Activity

--- main.py
+++ main.py
@@ -31,12 +31,12 @@ def home():
 <div class="message">
 {}
 </div>
-""".format(m.content)
+""".format(m.content.replace('<', '&lt;').replace('>', '&gt;'))
 
-    return body 
+    return body
 
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 6738))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='127.0.0.1', port=port)
 
